7e8dad48fc17fe40346f2f68764d8e9fb4defcf3
Vipul Solanki <157818450+VIPULbunny@users.noreply.github.com>
2025-01-09 23:57:14 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+x = np.array([0,1,2,3,4,5])
+y = np.array([0,10,20,30,40,50])
+
+# print(x,y)
+plt.plot(x,y)
+plt.show()
